b95c5ae0cc189cfdad9c37e8fe64c5583dfa1113
zfrahmd <zfr.ahmd5661@gmail.com>
2025-08-20 12:48:51 +0000

    Added prompt for deleting user

--- admin/manage-users.php
+++ admin/manage-users.php
@@ -91,7 +91,6 @@ $users=mysqli_query($connection,$query);
                             <td><?= $user["username"]  ?> </td>
                             <td class="text-center"><?= $user["is_admin"] ? 'Yes' : 'No'  ?></td>
                             <td class="text-center"><a href="<?= ROOT_URL ?>admin/edit-user.php?id=<?= $user['id'] ?>" class="btn btn-primary"><i class="fa fa-edit"></i>&nbsp; Edit</a></td>
-                            <td class="text-center"><a href="<?= ROOT_URL ?>admin/delete-users.php?id=<?= $user['id'] ?>" class="btn btn-danger"><i class="fa fa-trash" onclick="return confirm('Are you sure?');"></i>&nbsp;Delete</a></td>
                             <td class="text-center"><a href="<?= ROOT_URL ?>admin/delete-post.php?id=<?= $post['id'] ?>" class="btn btn-danger" onclick="return confirm('Are you sure?');"><i class="fa fa-trash"></i>&nbsp;Delete</a></td>
                         </tr>
                         <?php endwhile ?>
