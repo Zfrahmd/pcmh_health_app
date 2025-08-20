e68205b717bf99b47834b1efa63c0aa8794a7387
zfrahmd <zfr.ahmd5661@gmail.com>
2025-08-20 17:01:43 +0000

    Added form errors on edit user form

--- admin/edit-user-logic.php
+++ admin/edit-user-logic.php
@@ -9,17 +9,41 @@ if(!isset($_SESSION['user_is_admin'])){
 if(isset($_POST['submit'])){
     //get updated form data
     $id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
-    $firstname = filter_var($_POST['firstname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $firstname = (filter_var($_POST['firstname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
     $lastname = filter_var($_POST['lastname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
     $is_admin = filter_var($_POST['userrole'], FILTER_SANITIZE_NUMBER_INT);
+    $new_password = filter_var($_POST['newepassword'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $confirmpassword = filter_var($_POST['confirmpassword'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $header_string = 'admin/edit-user.php?id=' . strval($id);
 
-    //check for valid input
-    if(!$firstname || !$lastname ){
-        $_SESSION['edit-user'] = "Invalid form input on edit page";
-
+    //check for valid input, redirect back to edit-user on error
+    if(!$firstname){
+        $_SESSION['edit-user-data'] = 'Please enter your First Name';
+    }elseif(!$lastname){
+        $_SESSION['edit-user-data'] = 'Please enter your Last Name';
+    }elseif(!$email){
+        $_SESSION['edit-user-data'] = 'Please enter your Email';
+    }elseif(!($is_admin == 1 || $is_admin == 0 )){
+        $_SESSION['edit-user-data'] = 'Please select user role';
+    }elseif(strlen($new_password)<8 || strlen($confirmpassword)<8){
+        $_SESSION['edit-user-data'] = 'Password should be 8+ characters';
     }else{
+        if($new_password !== $confirmpassword){
+            $_SESSION['edit-user-data']="Passwords donot match";
+        }
+    }
+    // redirect back to edit-user on error
+    if(isset($_SESSION['edit-user-data'])){
+        // pass data back to edit user page
+        $_SESSION['edit-user-data'] = $_POST;
+        header("location: " . ROOT_URL . $header_string);
+        die();
+    }else{
+        $hashed_password = password_hash($new_password,PASSWORD_DEFAULT);
+
         //update user
-        $query = "UPDATE users SET firstname='$firstname', lastname='$lastname',is_admin=$is_admin WHERE id= '$id' LIMIT 1";
+        $query = "UPDATE users SET firstname='$firstname', lastname='$lastname', email='$email',is_admin='$is_admin',password='$hashed_password' WHERE id= '$id' LIMIT 1";
         $result = mysqli_query($connection, $query);
 
         if(mysqli_errno($connection)){
@@ -29,7 +53,6 @@ if(isset($_POST['submit'])){
             $_SESSION['edit-user-success'] = "User $firstname $lastname updated successfully";
 
         }
-
     }
 
 }
--- admin/edit-user.php
+++ admin/edit-user.php
@@ -9,13 +9,23 @@ if(!isset($_SESSION['user_is_admin'])){
 
 if(isset($_GET['id'])){
     $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
-    $query = "SELECT id,firstname,lastname,is_admin FROM users WHERE id='$id'";
+    $query = "SELECT id,firstname,lastname,email,is_admin FROM users WHERE id='$id'";
     $result = mysqli_query($connection, $query);
     $user = mysqli_fetch_assoc($result);
 }else{
     header('location: ' . ROOT_URL . 'admin/manage-users.php');
 }
 ?>
+<?php if(isset($_SESSION['edit-user-data'])): ?>
+    <div class="alert alert-danger alert-dismissible fade show alert-settings" role="alert">
+        <p>
+            <?=$_SESSION['edit-user-data'];
+            unset($_SESSION['edit-user-data']); 
+            ?>
+        </p>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+<?php endif ?>
 
 <section class="form__section">
     <div class="container form__section-container">
@@ -23,9 +33,11 @@ if(isset($_GET['id'])){
 
         <form action="<?= ROOT_URL ?>admin/edit-user-logic.php" class="centered_form" enctype="multipart/form-data" method="POST">
             <input class="form-control" type="hidden" name ="id" value="<?= $user['id'] ?>">
-            <input class="form-control" type="text" name ="firstname" value="<?= $user['firstname'] ?>"  placeholder="First Name">
-            <input class="form-control" type="text" name ="lastname"  value="<?= $user['lastname'] ?>"   placeholder="Last Name">
-            
+            <input class="form-control" type="text" name ="firstname" value="<?= $user['firstname'] ?>" placeholder="First Name">
+            <input class="form-control" type="text" name ="lastname"  value="<?= $user['lastname'] ?>" placeholder="Last Name">
+            <input class="form-control" type="email"    name ="email" value ="<?= $user['email']?>" placeholder="email">
+            <input class="form-control" type="password" name ="newepassword"  value =""  placeholder="New Password">
+            <input class="form-control" type="password" name ="confirmpassword" value =""  placeholder="New Password Confirm">
 
             <select name="userrole">
 				<option value="0">Author</option>
